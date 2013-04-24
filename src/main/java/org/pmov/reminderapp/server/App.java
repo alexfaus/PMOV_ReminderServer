339cbadff63ef7f1e3e485bf8a2e2ad578669dfd
Esteban Garcia <esteban@amglass.es>
2013-04-24 20:32:36 +0200

    Añadidos las clases que faltaban
    Hasta aquí está lo que se hizo en clase, el 23 de Abril de 2013

--- src/main/java/org/pmov/reminderapp/server/App.java
+++ src/main/java/org/pmov/reminderapp/server/App.java
@@ -1,13 +1,35 @@
 package org.pmov.reminderapp.server;
 
-/**
- * Hello world!
- *
- */
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class App 
 {
-    public static void main( String[] args )
+	// Variable final con el número de PUERTO de comunicaciones
+	public static final int PUERTO = 1234;
+	
+    public static void main( String[] args ) throws IOException
     {
-        System.out.println( "Hello World!" );
+    	// Declaro un ServerSocket y un Socket
+        ServerSocket socketServidor;
+        Socket socketConectado;
+        
+        // Nuevo ServerSocket
+        socketServidor = new ServerSocket(PUERTO);
+        
+        // Creamos un while para estar leyendo las conexiones que puedan hacerse
+        while(true)
+        {
+        	// Acepto una conexión y espero
+        	socketConectado = socketServidor.accept();
+        	
+        	// Mostramos mensaje de quién se ha conectado
+        	// ++ CODE HERE ++
+        	
+        	// Creamos un nuevo hilo
+        	new HiloServidor(socketConectado);
+        }
     }
 }
+
---
+++ src/main/java/org/pmov/reminderapp/server/HiloServidor.java
@@ -0,0 +1,64 @@
+package org.pmov.reminderapp.server;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class HiloServidor implements Runnable
+{
+	// Socket donde estamos conectados
+	private Socket elSocket;
+	
+	// Stream de recepción
+	private InputStream entrada;
+	
+	// Stream de envío
+	private OutputStream salida;
+
+	public HiloServidor(Socket con) throws java.io.IOException
+	{
+		// Guardo el socket que me envía la clase App
+		this.elSocket = con;
+		
+		// Obtengo los streamings de recepción y envío
+		this.entrada = con.getInputStream();
+		this.salida = con.getOutputStream();
+		
+		// Creamos un Thread para que ejecute el método run()
+		Thread th = new Thread(this);
+		th.start();
+	}
+	public void run()
+	{
+		try
+		{
+			// Obtenemos el flujo de recepción
+			String mensajeRecibidoSinTrocear = IO.leeLinea(entrada);
+			
+			// Troceamos la línea con split()
+			String[] mensajeRecibidoTroceado = mensajeRecibidoSinTrocear.split(" ");
+			
+			// Comprobamos el troceado
+			System.out.println("Trozo 0: " + mensajeRecibidoTroceado[0]);
+			System.out.println("Trozo 1: " + mensajeRecibidoTroceado[1]);
+			
+			// Mandamos un mensaje al cliente como mensaje recibido
+			IO.escribeLinea("Mensaje recibido", salida);
+			
+			// Cerramos los flujos
+			entrada.close();
+			salida.close();
+			
+			// Cerramos el Socket
+			elSocket.close();
+			
+			// Terminamos
+			return;
+			
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}	
+}
