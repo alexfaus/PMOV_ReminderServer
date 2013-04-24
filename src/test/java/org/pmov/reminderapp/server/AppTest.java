cd108cadede06ad8eea352dfde52ddf76ce350f2
Esteban Garcia <esteban@amglass.es>
2013-04-24 21:50:20 +0200

    Simulación cliente escrito en AppTest

--- src/test/java/org/pmov/reminderapp/server/AppTest.java
+++ src/test/java/org/pmov/reminderapp/server/AppTest.java
@@ -1,38 +1,62 @@
 package org.pmov.reminderapp.server;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest 
     extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
     public AppTest( String testName )
     {
         super( testName );
     }
-
-    /**
-     * @return the suite of tests being tested
-     */
+    
     public static Test suite()
     {
+    	// Creamos un thread
+    	Thread th = new Thread(new Runnable()
+    	{
+    		public void run()
+    		{
+    			try
+    			{
+    				App.main(null);
+    			} catch(IOException ex)
+    			{
+    				
+    			}
+    		}
+    	});
+    	
+    	th.start();
         return new TestSuite( AppTest.class );
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
+    public void testApp() throws UnknownHostException, IOException
     {
-        assertTrue( true );
+    	String hostServidor = "localhost";
+		int PUERTO = 1234;
+		
+		Socket elSocket = new Socket(hostServidor, PUERTO);
+		OutputStream salida;
+		InputStream entrada;
+		salida = elSocket.getOutputStream();
+		entrada = elSocket.getInputStream();
+		
+		IO.escribeLinea("POST ordenesEnviadas", salida);
+		
+		String mensajeRecibido = IO.leeLinea(entrada);
+		System.out.println(mensajeRecibido);
+		
+		salida.close();
+		entrada.close();
+		elSocket.close();
     }
 }
