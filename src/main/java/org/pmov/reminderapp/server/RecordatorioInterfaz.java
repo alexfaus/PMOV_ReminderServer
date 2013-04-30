daf3f1b147f2b35c0adb26b73891a32b9a796f7f
puntasso <pepitopuntasso@gmail.com>
2013-04-30 23:46:26 +0200

    Añadido metodo CerrarBD para no tener que abrir y cerrar la conexion a la BD en cada metodo GET POST DELETE...

--- src/main/java/org/pmov/reminderapp/server/RecordatorioInterfaz.java
+++ src/main/java/org/pmov/reminderapp/server/RecordatorioInterfaz.java
@@ -1,12 +1,14 @@
 package org.pmov.reminderapp.server;
 
 public interface RecordatorioInterfaz {
-	public String metodoPOST(String argumentos);
+	public void metodoPOST(String fecha, String asunto);
 	
 	public String metodoGET(String argumentos);
 	
 	public String metodoPUT(String argumentos);
 	
 	public String metodoDELETE(String argumentos);
+	
+	public void CerrarConexion();
 	
 }
